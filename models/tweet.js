2f05265de7f9019d18f48ed9917c4e982cf802b0
andysml <andy922200@gmail.com>
2019-09-18 00:33:27 +0100

    Set Database Association

--- models/tweet.js
+++ models/tweet.js
@@ -2,7 +2,13 @@
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
   }, {});
-  Tweet.associate = function(models) {
+  Tweet.associate = function (models) {
+    Tweet.belongsTo(models.User)
+    Tweet.belongsToMany(models.User, {
+      through: models.Like,
+      foreignKey: 'TweetId',
+      as: 'LikedUsers'
+    })
   };
   return Tweet;
 };

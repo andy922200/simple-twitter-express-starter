e3761e0a87664fff0b19c9ed39eff118ba79013b
Brian.L <brian9500885@gmail.com>
2019-09-18 16:36:52 +0800

    加一些datatypes到models裡

--- models/user.js
+++ models/user.js
@@ -1,8 +1,19 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
+  const User = sequelize.define(
+    'User',
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      name: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      introduction: DataTypes.TEXT,
+      role: DataTypes.STRING
+    },
+    {}
+  )
   User.associate = function(models) {
-  };
-  return User;
-};
+    User.hasMany(models.Tweet)
+  }
+  return User
+}

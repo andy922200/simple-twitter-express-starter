f08280f2bfd36b8d2f3be7f074840ffd5584be26
Jung Liu <realumbrally@gmail.com>
2019-09-26 19:21:07 +0800

    Fix Like model for test

--- models/like.js
+++ models/like.js
@@ -3,12 +3,18 @@ module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define(
     'Like',
     {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
       UserId: DataTypes.INTEGER,
       TweetId: DataTypes.INTEGER
     },
     {}
   )
-  Like.associate = function(models) {
+  Like.associate = function (models) {
     Like.belongsTo(models.User)
     Like.belongsTo(models.Tweet)
   }
